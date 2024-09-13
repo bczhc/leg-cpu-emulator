afa2f96c0da24e1bed713796f3019581898d32d0
Zhai Can <bczhc0@126.com>
2024-09-13 10:22:17 +0800

    tests: add emulator test for fibonacci.asm

--- tests/emulator.rs
+++ tests/emulator.rs
@@ -1,15 +1,15 @@
 use leg_cpu_emulator::assembler::Assembler;
 use leg_cpu_emulator::emulator::Emulator;
 
-fn main() {
+#[test]
+fn fibonacci() {
     let target = Assembler::new(include_str!("../tests/data/fibonacci.asm"))
         .unwrap()
         .assemble();
-    println!("{}", target.commented_binary);
 
     let binary = target.binary.merge();
     let mut emulator = Emulator::new(binary).unwrap();
-    
+
     loop {
         emulator.tick().unwrap();
         if emulator.halted {
@@ -17,7 +17,5 @@ fn main() {
         }
     }
 
-    for x in emulator.ram.iter().take(10) {
-        println!("{}", x);
-    }
+    assert_eq!(&emulator.ram[..10], &[1, 1, 2, 3, 5, 8, 13, 21, 34, 55]);
 }
